bb758318315758f0d95f0581533aaad300923872
GLFV <okolesnik753@gmail.com>
2022-10-27 20:51:58 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const ytdl = require('ytdl-core')
+const { getInfo } = require('ytdl-getinfo')
+const cors = require('cors')
+const fs = require('fs')
+const { resolve } = require('path')
+const app = express()
+
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+
+// app.get('/', async (req, res) => {
+// 	const url = req.query.URL
+// 	console.log(url)
+// 	await new Promise(resolve => {
+// 		ytdl(url)
+// 			.pipe(fs.createWriteStream('video.mp4'))
+// 			.on('close', () => {
+// 				resolve()
+// 			})
+// 	})
+// 	console.log('JEKa')
+// 	res.download(__dirname + '/video.mp4')
+// })
+
+app.get('/', async (req, res) => {
+	const url = req.query.URL
+	let obj = {}
+	getInfo(url)
+		.then(info => {
+			info.items[0].formats.forEach(format => {
+				if (format.format_note > '0' && format.format_note < '9') {
+					obj[format.format_note] = true
+				}
+			})
+
+			delete obj['tiny']
+			res.send([obj, info.items[0].webpage_url])
+		})
+		.catch(err => res.status(500).send('error: ' + err))
+})
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`)
+})
